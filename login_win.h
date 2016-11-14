09e085bf236664339aa6d7d7b8c16fb43505dd20
nathanjbrockway <ninjabrockway@gmail.com>
2016-11-14 09:23:04 -0600

    added login_window header file

---
+++ login_win.h
@@ -0,0 +1,11 @@
+/*Brockway, Khatiri, Tiner
+Final Project
+Last update: 11-14-16
+Purpose: header file for login window--first window in the gui design*/
+
+#include "Window.h"
+#include "GUI.h"
+
+class login_window{
+	//...
+}

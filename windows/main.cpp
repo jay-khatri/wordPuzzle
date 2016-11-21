51db4321af4b5d8290b0847e958af2de77fc88e4
ntiner1 <ntiner1@tamu.edu>
2016-11-21 11:53:34 -0600

    messed with the main, won't keep tho

--- windows/main.cpp
+++ windows/main.cpp
@@ -3,21 +3,32 @@
 #include "login_window.h"
 #include "game_window.h"
 
-int main(){
+void run_login(){
 	try{
 		login_window win(Point(100,100), 400, 200, "login");
 		return gui_main();
 	}
 	catch(...){
 		cout << "Something went wrong\n";
-		return 1;
+		return;
 	}
+}
+
+void run_game(){
 	try{
 		Game_window win2(Point(100,100), 600, 400, "gameplay");
 		return gui_main();
 	}
 	catch(...){
 		cout << "Something went wrong\n";
-		return 1;
+		return;
 	}
+}
+
+int main(){
+	
+	run_login();
+
+	run_game();
+
 }

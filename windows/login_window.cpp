fa9db0a6ceffb9715cd68d6b0da1d2077c539dbd
nathanjbrockway <ninjabrockway@gmail.com>
2016-11-28 12:15:32 -0600

    made changes for login

--- windows/login_window.cpp
+++ windows/login_window.cpp
@@ -32,18 +32,63 @@ void login_window::new_user_pressed(){
 int login_window::enter_pressed(int i){
 	string n = name_in.get_string();
 	//check/make a player here:
-	if (n=="nate" && i==0){//bool function for checking name
-		attach(dne_message);
+	switch (i){
+		case 0:{
+			if (!isPerson(n, peeps)){//bool function for checking name
+				attach(dne_message); // didn't find player (bad)
+				cout << "found name: " << n << ".\n";//test messages
+				redraw();
+			}
+			else{
+				cout << name_in.get_string() << "(reference)\n";//get reference in vector
+		
+				quit_pressed();
+				try{
+					Game_window win2(Point(100,100), 600, 400, "gameplay");
+					return gui_main();
+				}
+				catch(...){
+					cout << "Something went wrong\n";
+					return 1;
+				}
+			}
+			break;
+		}
+		case 1:{
+			if (isPerson(n, peeps)){
+				attach(exist_message); // found player (bad)
+				cout << "found name: " << n << ".\n";//test messages
+				redraw();
+			}
+			else{
+				cout << name_in.get_string() << "(creating)\n";//in actual program push back player here.
+		
+				quit_pressed();
+				try{
+					Game_window win2(Point(100,100), 600, 400, "gameplay");
+					return gui_main();
+				}
+				catch(...){
+					cout << "Something went wrong\n";
+					return 1;
+				}
+			}
+			break;
+		}
+	}
+	if (!isPerson(n, peeps) && i==0){//bool function for checking name
+		attach(dne_message); // didn't find player (bad)
 		cout << "found name: " << n << ".\n";//test messages
 		redraw();
 	}
-	else if (n=="nick" && i==1){
-		attach(exist_message);
+	else if (isPerson(n, peeps) && i==1){
+		attach(exist_message); // found player (bad)
 		cout << "found name: " << n << ".\n";//test messages
 		redraw();
 	}
 	else{
 		cout << name_in.get_string() << '\n';//in actual program push back player here.
+		
 		quit_pressed();
 		try{
 			Game_window win2(Point(100,100), 600, 400, "gameplay");
@@ -52,7 +97,7 @@ int login_window::enter_pressed(int i){
 		catch(...){
 			cout << "Something went wrong\n";
 			return 1;
-		}//in actual program call game_window here
+		}
 	}
 
 }

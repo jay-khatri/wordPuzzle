899025d44bae54fe2f62d4e70bb8162ea44a9951
ntiner1 <ntiner1@tamu.edu>
2016-12-02 22:33:11 -0600

    the default pic now works

--- windows/end_window.cpp
+++ windows/end_window.cpp
@@ -8,6 +8,49 @@ Purpose: function and class definitons for end window--third window in a gui seq
 using namespace Graph_lib;
 using namespace std;
 
+bool is_pic(string pic){
+	if(pic.substr(pic.length()-4,4)==".jpg" || pic.substr(pic.length()-4,4)==".gif"){
+		return 1;
+	}
+	return 0;
+}
+
+bool is_open_pic(string pic){
+	ifstream is{pic};
+	if(!is){
+		return 0;
+	}
+	return 1;
+}
+
+string get_place_name(vector<Person> peeps, int gametype, int place){
+	vector<Person> topvec;
+	vector<string> places;
+	//gets the index of the person with the highest score
+	for(int i = 0; i<3 & peeps.size() > 0; i++){
+		topvec.push_back(peeps[topIndex(peeps, gametype)]);
+		peeps.erase(peeps.begin() + topIndex(peeps, gametype));
+	}
+	for(int i = 0; i<topvec.size(); i++){
+		// string str_place = to_string(i+1);
+		// string str_score = to_string(topvec[i].getScores(gametype)[2]);
+		string str_name = topvec[i].getName();
+		// string fin_string = str_name;
+		places.push_back(str_name);
+	}
+	if (place > 0 && place < 4 && gametype > 2 && gametype < 6){
+		//size is 2, place is 1,2,3 (3 should fail)
+		if(topvec.size()>=place){ 
+			return places[place-1];
+		}
+		else{
+			return "Score DNE";
+		}
+	}else{
+		return "You doing something wrong";
+	}
+}
+
 end_window::end_window(Point xy, int w, int h, const string& title, vector<Person>& people, Person& pp, int score, int gt, int winner): 
 Window(xy, w, h, title), peeps(people), the_player(pp), 
 final_score(score), game_type(gt),
@@ -36,21 +79,36 @@ top_msg(Point(110,90),"Top Player: "),
 high_scores_msg(Point(90,215), game_string(game_type) + "High Scores"),
 
 enter_pic_msg1(Point(x_max()-170, y_max()/2 + 20), "Enter the picture file"),
-enter_pic_msg2(Point(x_max()-170, y_max()/2 + 35), "   (*.jpg or *.gif)"),
+enter_pic_msg2(Point(x_max()-170, y_max()/2 + 35), "   (*.jpg or *.gif)")
 
-//other initializations
-pic_file("default.jpg")
 {
-	//the_player.addScore(gt, score);
+
 	//if statement getting the already top player's pic and reassigning the Image.
+	string top_in = get_place_name(peeps, game_type, 1);
+	pic_file = getPerson(top_in, peeps).getPic();
+
+	if(is_pic(pic_file)){
+		if(is_open_pic(pic_file)){
+			//the pic is fine if reaches here
+		}
+		else{
+			pic_file = "default.jpg";
+		}
+	}
+	else{
+		pic_file = "default.jpg";
+	}
+	
+	Image* proxy = new Image(Point(100,100), pic_file);
+	top_pic = proxy;
+
 	for(int i=0; i<the_player.getScores(game_type).size();++i){
 		cout << the_player.getScores(game_type)[i] << endl;
 	}
 	cout << peeps.size() << endl;
 	cout << getPlace(peeps, game_type, 1) << endl;
 
-	Image* proxy = new Image(Point(100,100), pic_file);
-	top_pic = proxy;//ugh...^
+	
 	
 	attach(play_again);
 	attach(logout);
@@ -102,21 +160,6 @@ string game_string(int i){
 			return "ERROR: ";
 		}
 	}
-}
-
-bool is_pic(string pic){
-	if(pic.substr(pic.length()-4,4)==".jpg" || pic.substr(pic.length()-4,4)==".gif"){
-		return 1;
-	}
-	return 0;
-}
-
-bool is_open_pic(string pic){
-	ifstream is{pic};
-	if(!is){
-		return 0;
-	}
-	return 1;
 }
 
 void end_window::enter_pic_pressed(){
@@ -217,3 +260,12 @@ void output_people(vector<Person> input) {
 string Person::getPic(){ return pic;}
 
 void Person::setPic(string i_pic){ pic = i_pic;}
+
+Person &getPerson(string name, vector<Person> &peeps){
+	for(int i = 0; i<peeps.size(); i++){
+		if (peeps[i].getName().find(name) != string::npos) {
+			return peeps[i];
+		}
+	}
+}	
+
